5d86c380ad8813a54fd35107c7ea5c8102a2938f
Om Golhani <omgolhani3604@gmail.com>
2023-05-31 20:51:17 +0530

    added expiry alerts

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -8,6 +8,7 @@ export default function NoteItem(props) {
     const { deleteNote } = context;
 
     const { note, updateNote, shareNote } = props;
+    //console.log(note.expdate);
 
     let addday = note.date.substr(8, 2);
     let addmonth = note.date.substr(5, 2);
@@ -17,26 +18,25 @@ export default function NoteItem(props) {
     let expmonth = note.expdate.substr(5, 2);
     let expyear = note.expdate.substr(0, 4);
 
-    const date = new Date();
-    let curryear = date.getFullYear();
-    let currmonth = date.getMonth() + 1;
-    let currday = date.getDate();
+    const date1 = new Date();
+    let curryear = date1.getFullYear();
+    let currmonth = date1.getMonth() + 1;
+    let currday = date1.getDate();
 
-    let flag = false;
-    if (expyear < curryear) {
-        flag = true;
-    }
-    else if (expyear == curryear && expmonth < currmonth) {
-        flag = true;
-    }
-    else if (expmonth == currmonth && expday < currday) {
-        flag = true;
-    }
+    var date2 = new Date(`${expmonth}/${expday}/${expyear}`);
+    // console.log(date1.getTime());
+    // console.log(date2.getTime());
+
+    var time_difference = date2.getTime() - date1.getTime();
+
+    //calculate days difference by dividing total milliseconds in a day  
+    var days_difference = time_difference / (1000 * 60 * 60 * 24);
+    days_difference = Math.ceil(days_difference);
 
     return (
         <div className='col-md-3'>
             <div className=" card notecard text-center my-3">
-                <div className='card-header d-flex justify-content-between align-items-center rounded' style={{ backgroundColor: flag == true ? "#ffb8b8" : "#A5D7E8" }}>
+                <div className='card-header d-flex justify-content-between align-items-center rounded' style={{'backgroundColor': days_difference<0? '#FA9884' : '#A5D7E8'}}>
                     <h5 className="card-title text-start" >{note.title}</h5>
                     <div className="dropdown">
                         <a
@@ -48,6 +48,7 @@ export default function NoteItem(props) {
                             <i className="fas fa-ellipsis-v fa-lg text-dark px-1"></i>
                         </a>
                         <ul className="dropdown-menu" aria-labelledby="dropdownMenuicon">
+
                             <li><button className="dropdown-item text-center" onClick={() => {
                                 updateNote(note)
                             }}> <i className="fa-solid fa-pen-to-square fa-sm px-1"></i> Edit</button></li>
@@ -60,6 +61,7 @@ export default function NoteItem(props) {
                             <li><button className="dropdown-item text-center" onClick={() => {
                                 shareNote(note)
                             }}><i className="fa-sharp fa-solid fa-share fa-sm px-1"></i> Share</button></li>
+
                         </ul>
                     </div>
                 </div>
@@ -73,17 +75,12 @@ export default function NoteItem(props) {
                     <strong>Expiry </strong>: {expday} - {expmonth} - {expyear}
                 </div>
                 <div className="card-footer text-muted">
-                    {(currmonth == expmonth &&
-                        curryear == expyear &&
-                        (expday - currday) <= 7 &&
-                        (expday - currday > 0)) &&
-                        <div className="timer mb-2" style={{ color: "#c03535" }}><strong> {expday - currday}d </strong>
-                            <i className="fa-sharp fa-solid fa-clock fa-xl" style={{ color: "#c03535" }}> </i>
+                    {days_difference >= 0 &&
+                        <div className="timer mb-2" style={{ color: "#367E18" }}><strong> {days_difference} days left </strong>
+                            <i className="fa-sharp fa-solid fa-clock fa-xl" style={{ color: "#367E18" }}> </i>
                         </div>
                     }
-                    {(currmonth == expmonth &&
-                        curryear == expyear &&
-                        (expday - currday <= 0)) &&
+                    { days_difference<0 &&  
                         <div className="timer mb-2" style={{ color: "#c03535" }}>
                             <i className="fa-sharp fa-solid fa-circle-xmark fa-xl" style={{ color: "#c03535" }}> </i><strong>
                                 &nbsp; Expired </strong>
